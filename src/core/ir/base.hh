f2bfeab35421e259ef57e247f4827ddc73ccb6d7
David A. Greene <greened@smith.obbligato.org>
2009-08-15 16:38:30 -0500

    Bring repository up to date with latest sources

---
+++ src/core/ir/base.hh
@@ -0,0 +1,10 @@
+#ifndef MIRVBase_hh
+#define MIRVBase_hh
+
+namespace MIRV {
+   // Signifies the root of some IR class subtree,
+   // e.g. Expression<Base>, Statement<Base>, etc.
+   struct Base {};
+}
+
+#endif
