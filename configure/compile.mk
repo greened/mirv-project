c58193b9a3ab41245e6521509c6cc97372e65b24
David A. Greene <greened@waller.obbligato.org>
2009-11-26 00:50:00 -0600

    More build fixes.

--- configure/compile.mk
+++ configure/compile.mk
@@ -5,64 +5,60 @@ include $(BUILDTOOLS)/configure/try.mk
 include $(BUILDTOOLS)/configure/define.mk
 include $(BUILDTOOLS)/configure/bind.mk
 
-define mc_compile_text
+define mc_compile_impl
 
-$$(call debug,mc_compile_impl("$(1)" "$(2)" "$(3)"))
+$$(call debug,[mc_compile_impl] mc_compile_impl("$(1)" "$(2)" "$(3)" "$(4)" "$(5)"))
 
 $(1)_OSRCFILE := $(FINAL_BUILDDIR)/configure/$(1)_compile_test.$(3)
 $(1)_OFILE := $$(patsubst %.$(3),%.o,$$($(1)_OSRCFILE))
 
 $(1)_compile_source := $$($(2))
 
-$$(call debug,[mc_compile_text] $(1)_OSRCFILE = $$($(1)_OSRCFILE))
-$$(call debug,[mc_compile_text] $(1)_OFILE    = $$($(1)_OFILE))
+$$(call debug,[mc_compile_impl] $(1)_OSRCFILE = $$($(1)_OSRCFILE))
+$$(call debug,[mc_compile_impl] $(1)_OFILE    = $$($(1)_OFILE))
 
 .PRECIOUS: $$($(1)_OSRCFILE)
 $$($(1)_OSRCFILE):
 	$(QUIET)echo "$$($(1)_compile_source)" > $$@
 
-$$(call debug,[mc_compile_text] $$($(1)_OFILE): $$($(1)_OSRCFILE) $(FINAL_BUILDDIR)/configure/$$(firstword $(1)).mk)
+$$(call debug,[mc_compile_impl] $$($(1)_OFILE): $$($(1)_OSRCFILE) $(5))
+
+$$(if $(5),include $(5))
 
 .PRECIOUS: $$($(1)_OFILE)
 .SECONDEXPANSION:
-$$($(1)_OFILE): $$($(1)_OSRCFILE) $(4)
+$$($(1)_OFILE): $$($(1)_OSRCFILE) $(5)
+	$$(if $(5),$$(eval include $(5)))
 	-$(QUIET)$$($(1)) -c $$< -o $$@
 
-$$(call debug,[mc_compile_text] $(FINAL_BUILDDIR)/configure/$(1)_compile.mk: $$($(1)_OFILE))
+$$(call debug,[mc_compile_impl] $(4): $$($(1)_OFILE))
 
-$(FINAL_BUILDDIR)/configure/$(1)_compile_target: $$($(1)_OFILE)
-	$(QUIET)$$(if $$(wildcard $$($(1)_OFILE)),$$(call mc_define,$(1)_compile,yes,$$@),$$(call mc_define,$(1)_compile,,$$@))
+$(4): $$($(1)_OFILE)
+	$(QUIET)$$(if $$(wildcard $$(<),$$(call mc_define,$(1)_compile,yes,$$@),$$(call mc_define,$(1)_compile,,$$@))
 
-endef
-
-define mc_compile_impl
-
-$$(eval $$(call mc_compile_text,$(1),$(2),$(3),$(4)))
-
-.SECONDEXPANSION:
-$(FINAL_BUILDDIR)/configure/$(1)_compile.mk: $(FINAL_BUILDDIR)/configure/$(1)_compile_target
-	$(QUIET)echo 'include $(FINAL_BUILDDIR)/configure/$(1)_compile_target' > $$@
-
-CONFIGURE_INCLUDES += $(FINAL_BUILDDIR)/configure/$(1)_compile.mk
-CONFIGURE_TARGETS += $(FINAL_BUILDDIR)/configure/$(1)_compile_target
+CONFIGURE_INCLUDES += $(4)
 
 endef
 
 # $1: The name of the variable holding the compiler location
 # $2: The source code to compile
 # $3: The source code file extension
-# $4: Dependency
+# $4: Output file to create
+# $5: Dependency
 # Set $(1)_compile to yes or empty
-mc_compile = $(eval $(call mc_compile_impl,$(1),$(2),$(3),$(4)))
-
+mc_compile = $(eval $(call mc_compile_impl,$(1),$(2),$(3),$(4),$(5)))
 
 # $1: The source code to compile
+# $2: Output file to create
+# $3: Dependence
 # Set CC_compile to yes or empty
-mc_compile_c = $(call mc_compile,CC,$(1),c,$(FINAL_BUILDDIR)/configure/CC.mk)
+mc_compile_c = $(call mc_compile,CC,$(1),c,$(2),$(3))
 
 # $1: The source code to compile
+# $2: Output file to create
+# $3: Dependence
 # Set CXX_compile to yes or empty
-mc_compile_cxx = $(call mc_compile,CXX,$(1),cc,$(FINAL_BUILDDIR)/configure/CXX.mk)
+mc_compile_cxx = $(call mc_compile,CXX,$(1),cc,$(2),$(3))
 
 # $1: The name of the variable holding the compiler location
 # $2: The source code to compile
@@ -70,18 +66,21 @@ mc_compile_cxx = $(call mc_compile,CXX,$(1),cc,$(FINAL_BUILDDIR)/configure/CXX.m
 # $4: False action
 # $5: The source code file extension
 # $6: Include file to create (actions must create it)
-mc_try_compile = $(call mc_try,$(1)_compile,$(call bind3,$(1)_compile_bind,mc_compile,$(1),$(2),$(5)),$(3),$(4),$(6))
+# $7: Dependence
+mc_try_compile = $(call mc_try,$(1)_compile,$(call bind5,$(1)_compile_bind,mc_compile,$(1),$(2),$(5),$(patsubst %.mk,%_compile.mk,$(6)),$(7)),$(3),$(4),$(6),$(patsubst %.mk,%_compile.mk,$(6)))
 
 # $1: The source code to compile
 # $2: True action
 # $3: False action
 # $4: Include file to create (actions must create it)
-mc_try_compile_c = $(call mc_try,CC_compile,$(call bind1,CC_compile_bind,mc_compile_c,$(1)),$(2),$(3),$(4))
+# $5: Dependence
+mc_try_compile_c = $(call mc_try,CC_compile,$(call bind3,CC_compile_bind,mc_compile_c,$(1),$(patsubst %.mk,%_compile.mk,$(4)),$(5)),$(2),$(3),$(4),$(patsubst %.mk,%_compile.mk,$(4)))
 
 # $1: The source code to compile
 # $2: True action
 # $3: False action
 # $4: Include file to create (actions must create it)
-mc_try_compile_cxx = $(call mc_try,CXX_compile,$(call bind1,CXX_compile_bind,mc_compile_cxx,$(1)),$(2),$(3),$(4))
+# $5: Dependence
+mc_try_compile_cxx = $(call mc_try,CXX_compile,$(call bind3,CXX_compile_bind,mc_compile_cxx,$(1),$(patsubst %.mk,%_compile.mk,$(4)),$(5)),$(2),$(3),$(4),$(patsubst %.mk,%_compile.mk,$(4)))
 
 endif
--- configure/link.mk
+++ configure/link.mk
@@ -6,80 +6,75 @@ include $(BUILDTOOLS)/configure/define.mk
 include $(BUILDTOOLS)/configure/bind.mk
 include $(BUILDTOOLS)/configure/ls.mk
 
-define mc_link_text
+define mc_link_impl
 
-$$(call debug,[mc_link_text] mc_link_impl("$(1)" "$(2)" "$(3)"))
+$$(call debug,[mc_link_impl] mc_link_impl("$(1)" "$(2)" "$(3)" "$(4)" "$(5)"))
 
 $(1)_LDOBJFILE := $(2)
 $(1)_LDFILE := $$(patsubst %.o,%.exe,$$($(1)_LDOBJFILE))
 
-$$(call debug,[mc_link_text] $(1)_LDOBJFILE = $$($(1)_LDOBJFILE))
-$$(call debug,[mc_link_text] $(1)_LDFILE    = $$($(1)_LDFILE))
+$$(call debug,[mc_link_impl] $(1)_LDOBJFILE = $$($(1)_LDOBJFILE))
+$$(call debug,[mc_link_impl] $(1)_LDFILE    = $$($(1)_LDFILE))
 
-$$(call debug,[mc_link_text] $$($(1)_LDFILE): $$($(1)_LDOBJFILE) $(FINAL_BUILDDIR)/configure/$$(firstword $(1)).mk)
+$$(call debug,[mc_link_impl] $$($(1)_LDFILE): $$($(1)_LDOBJFILE) $(5))
+
+$$(if $(5),include $(5))
 
 .PRECIOUS: $$($(1)_LDFILE)
 .SECONDEXPANSION:
-$$($(1)_LDFILE): $$($(1)_LDOBJFILE) $(4)
+$$($(1)_LDFILE): $$($(1)_LDOBJFILE) $(5)
+	$$(if $(5),$$(eval include $(5)))
 	-$(QUIET)$$($(1)) $$< $(3) -o $$@
 
-$$(call debug,[mc_link_text] $(FINAL_BUILDDIR)/configure/$(1)_link_target: $$($(1)_LDFILE))
+$$(call debug,[mc_link_impl] $(4): $$($(1)_LDFILE))
 
-$(FINAL_BUILDDIR)/configure/$(1)_link_target: $$($(1)_LDFILE)
-	$(QUIET)if $$(LS) $$(<); then \
-	$(QUIET)  $$(call mc_define,$(1)_link,yes,$$@); \
-	$(QUIET)else \
-	$(QUIET)  $$(call mc_define,$(1)_link,,$$@); \
-	$(QUIET)fi
+$(4): $$($(1)_LDFILE)
+	$(QUIET)$$(if $$(wildcard $$(<),$$(call mc_define,$(1)_compile,yes,$$@),$$(call mc_define,$(1)_compile,,$$@))
 
 endef
 
-define mc_link_impl
-
-$$(eval $$(call mc_link_text,$(1),$(2),$(3),$(4)))
-
-.SECONDEXPANSION:
-$(FINAL_BUILDDIR)/configure/$(1)_link.mk: $(FINAL_BUILDDIR)/configure/$(1)_link_target
-	$(QUIET)echo 'include $(FINAL_BUILDDIR)/configure/$(1)_link_target' > $$@
-
-CONFIGURE_INCLUDES += $(FINAL_BUILDDIR)/configure/$(1)_link.mk
-CONFIGURE_TARGETS += $(FINAL_BUILDDIR)/configure/$(1)_link_target
-
-endef
+CONFIGURE_INCLUDES += $(4)
 
 # $1: The name of the variable holding the linker location
 # $2: The object file to link
 # $3: Additional linker flags (libraries, etc.)
-# $4: Dependency
+# $4: Output file to create
+# $5: Dependency
 # Set $(1)_link to yes or empty
-mc_link = $(eval $(call mc_link_impl,$(1),$(2),$(3),$(4)))
-
+mc_link = $(eval $(call mc_link_impl,$(1),$(2),$(3),$(4),$(5)))
 
 # $1: The object file to link
+# $2: Output file to create
+# $3: Dependence
 # Set CC_link to yes or empty
-mc_link_c = $(call mc_link,CC,$(1),,$(FINAL_BUILDDIR)/configure/CC.mk)
+mc_link_c = $(call mc_link,CC,$(1),,$(2),$(3))
 
 # $1: The object file to link
+# $2: Output file to create
+# $3: Dependence
 # Set CXX_link to yes or empty
-mc_link_cxx = $(call mc_link,CXX,$(1),,$(FINAL_BUILDDIR)/configure/CXX.mk)
+mc_link_cxx = $(call mc_link,CXX,$(1),,$(2),$(3))
 
 # $1: The name of the variable holding the linker location
 # $2: The object file to link
 # $3: True action
 # $4: False action
 # $5: Include file to create (actions must create it)
-mc_try_link = $(call mc_try,$(1)_link,$(call bind2,$(1)_link_bind,mc_link,$(1),$(2)),$(3),$(4),$(5))
+# $6: Dependence
+mc_try_link = $(call mc_try,$(1)_link,$(call bind5,$(1)_link_bind,mc_link,$(1),$(2),,$(patsubst %.mk,%_link.mk,$(5)),$(6)),$(3),$(4),$(patsubst %.mk,%_link.mk,$(5)))
 
 # $1: The object file to link
 # $2: True action
 # $3: False action
 # $4: Include file to create (actions must create it)
-mc_try_link_c = $(call mc_try,CC_link,$(call bind1,CC_link_bind,mc_link_c,$(1)),$(2),$(3),$(4))
+# $5: Dependence
+mc_try_link_c = $(call mc_try,CC_link,$(call bind3,CC_link_bind,mc_link_c,$(1),$(patsubst %.mk,%_link.mk,$(4)),$(5)),$(2),$(3),$(4),$(patsubst %.mk,%_link.mk,$(4)))
 
 # $1: The object file to link
 # $2: True action
 # $3: False action
 # $4: Include file to create (actions must create it)
-mc_try_link_cxx = $(call mc_try,CXX_link,$(call bind1,CXX_link_bind,mc_link_cxx,$(1)),$(2),$(3),$(4))
+# $5: Dependence
+mc_try_link_cxx = $(call mc_try,CXX_link,$(call bind3,CXX_link_bind,mc_link_cxx,$(1),$(patsubst %.mk,%_link.mk,$(4)),$(5)),$(2),$(3),$(4),$(patsubst %.mk,%_link.mk,$(4)))
 
 endif
