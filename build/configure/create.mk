819a8fdc6b4f59e261b1b6bf3834340a6aac8a5b
David A. Greene <greened@waller.obbligato.org>
2009-11-29 22:44:14 -0600

    Build fixes.  Configure works.

--- build/configure/create.mk
+++ build/configure/create.mk
@@ -3,7 +3,7 @@ CREATE_MK := 1
 
 # $1: File to create
 # $2: Dependence
-mc_create_start = echo "ifndef $(subst .,_,$(subst /,_,$(1)))" > $(1) && echo "$(subst .,_,$(subst /,_,$(1))) := 1" >> $(1) && echo >> $(1) $(if $(2),&& echo "include $(2)" >> $(1) && echo >> $(1))
+mc_create_start = echo "ifndef $(subst .,_,$(subst /,_,$(1)))" > $(1) && echo "$(subst .,_,$(subst /,_,$(1))) := 1" >> $(1) && echo >> $(1) && echo "include $(BUILDTOOLS)/configure/configure.mk" >> $(1) $(if $(2),&& echo "include $(2)" >> $(1) && echo >> $(1))
 
 # $1: File to create
 mc_create_end = echo >> $(1) && echo "endif  \# $(subst .,_,$(subst /,_,$(1)))" >> $(1)
