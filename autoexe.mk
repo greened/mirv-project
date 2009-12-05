034e41d1aa721716fdbf549aca741c2fc3a01227
David A. Greene <greened@waller.obbligato.org>
2009-12-04 18:49:43 -0600

    Move include link so filters appear as if they are installed.

--- autoexe.mk
+++ autoexe.mk
@@ -3,13 +3,15 @@ AUTOEXE_MK := 1
 
 define make_exe_impl
 
+$$(call debug,[exe] '$(1)' '$(2)' '$(3)' '$(4)' '$(5)' '$(6)')
+
 $(1): LDFLAGS += $(foreach lib,$(3),$(patsubst lib%.a,-l%,$(firstword $(patsubst /, ,$$(lib))))) $(5)
 
 $$(call debug,[exe] $(1): LDFLAGS += $(foreach lib,$(3),$(patsubst lib%.a,-l%,$(firstword $(patsubst /, ,$$(lib))))) $(5)) 
 
-$$(call debug,[exe] $(1): $(2) $(3))
+$$(call debug,[exe] $(1): $(2) $(3) $(6))
 
-$(1): $(2) $(3)
+$(1): $(2) $(3) $(6)
 	$$($(4)) -o $$(@) $$(filter %.o,$$(^)) $$(LDFLAGS)
 
 endef
@@ -19,12 +21,14 @@ endef
 # $3: Libraries needed to create the executable
 # $4: The linker
 # $5: Additional linker flags
-make_exe = $(eval $(call make_exe_impl,$(1),$(2),$(3),$(4),$(5)))
+# $6: Dependency
+make_exe = $(eval $(call make_exe_impl,$(1),$(2),$(3),$(4),$(5),$(6)))
 
 # $1: The object making up the executable
 # $2: Libraries needed to create the executable
 # $3: The linker
 # $4: Additional linker flags
-make_simple_exe = $(call make_exe,$(patsubst %.o,%.exe,$(1)),$(1),$(2),$(3),$(4))
+# $5: Dependency
+make_simple_exe = $(call make_exe,$(patsubst %.o,%.exe,$(1)),$(1),$(2),$(3),$(4),$(5))
 
 endif
