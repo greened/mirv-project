b6b109b39e69bdccb51ce037c77ee55ae573ae4e
David A. Greene <greened@obbligato.org>
2009-12-05 15:58:45 -0600

    Fix filter interface to take a pointer to the visited type.

--- src/core/filter/filter.hh
+++ src/core/filter/filter.hh
@@ -1,13 +1,15 @@
 #ifndef mirv_core_filter_filter_hh
 #define mirv_core_filter_filter_hh
 
+#include <mirv/core/mem/heap.hh>
+
 namespace mirv {
    template<typename Visited, typename Result = void>
    class Filter {
    public:
       typedef Result result_type;
 
-      virtual result_type operator()(Visited &node) = 0;
+     virtual result_type operator()(typename ptr<Visited>::type node) = 0;
    };
 }
 
