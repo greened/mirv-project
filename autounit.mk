a198c255ec4579331978245eef4c5871fd973d8d
David A. Greene <greened@waller.obbligato.org>
2009-12-04 18:28:11 -0600

    Fix autoexe arguments.  Testcases now get compiles.

--- autounit.mk
+++ autounit.mk
@@ -11,20 +11,7 @@ define make_unittest_impl
 
 $$(call make_objects,$(1),$(2),$(4),$(5),$(6))
 
-$$(foreach obj,$$($(1)_OBJS),$$(call make_simple_exe,$(1),$$(obj),$(3),$(7),$(8)))
-
-%.out: %.exe
-	$(QUIET)$$(<) > $$(@)
-
-%.cf: %.c
-	$(QUIET)$(GREP) "STDOUT" $(<) | $(CUT) -d' ' -f2- > $$(@)
-
-%.result: %.out, %.cf
-	$(QUIET)if $(CMP) $$(*).out $$(*).cf; then \
-	          $(QUIET)echo "PASS: $$(*).exe" | $(TEE) $$(@); \
-	        else \
-	          $(QUIET)echo "FAIL: $$(*).exe" | $(TEE) $$(@); \
-	        fi
+$$(foreach obj,$$($(1)_OBJS),$$(call make_simple_exe,$$(obj),$(3),$(7),$(8)))
 
 $(1)_UNITTESTS += $$(patsubst %.o,%.result,$$($(1)_OBJS))
 
@@ -39,5 +26,18 @@ endef
 # $7: The linker
 # $8: Linker flags
 make_unittest = $(eval $(call make_unittest_impl,$(1),$(2),$(3),$(4),$(5),$(6),$(7),$(8)))
+
+%.out: %.exe
+	$(QUIET)$$(<) > $$(@)
+
+%.cf: %.cc
+	$(QUIET)$(GREP) "STDOUT" $(<) | $(CUT) -d' ' -f2- > $$(@)
+
+%.result: %.out %.cf
+	$(QUIET)if $(CMP) $$(*).out $$(*).cf; then \
+	          $(QUIET)echo "PASS: $$(*).exe" | $(TEE) $$(@); \
+	        else \
+	          $(QUIET)echo "FAIL: $$(*).exe" | $(TEE) $$(@); \
+	        fi
 
 endif
