c14c77a74a4c9806371f8d9024b774636accc184
David A. Greene <greened@waller.obbligato.org>
2009-12-14 08:30:08 -0600

    Re-arrange inner-node implementation.
    Provide an abstract base class for inner nodes so that the ABC
    can be virtually inherited.  Because the ABC has a default
    constructor, this avoids the need for the most-derived class
    to invoke the virtual base class constructor explicitly.

--- src/core/ir/inherit.hh
+++ src/core/ir/inherit.hh
@@ -16,14 +16,32 @@ namespace mirv {
   };
 
   namespace detail {
-    template<typename Visitor, typename T1, typename T2>
-    class Type : public T1, public T2 {
+    template<typename Visitor, typename T1>
+    class Type1 : public T1 {
     public:
+      Type1(void) {}
+
       template<typename A1>
-      Type(A1 a1) : T1(a1) {}
+      Type1(A1 a1) : T1(a1) {}
 
       template<typename A1, typename A2>
-      Type(A1 a1, A2 a2) : T1(a1, a2) {}
+      Type1(A1 a1, A2 a2) : T1(a1, a2) {}
+
+      void accept(Visitor &V) {
+	error("Inherit::accept called");
+      }
+    };
+
+    template<typename Visitor, typename T1>
+    class Type1<Visitor, Virtual<T1> > : public virtual T1 {
+    public:
+      Type1(void) {}
+
+      template<typename A1>
+      Type1(A1 a1) : T1(a1) {}
+
+      template<typename A1, typename A2>
+      Type1(A1 a1, A2 a2) : T1(a1, a2) {}
 
       void accept(Visitor &V) {
 	error("Inherit::accept called");
@@ -31,14 +49,32 @@ namespace mirv {
     };
 
     template<typename Visitor, typename T1, typename T2>
-    class Type<Visitor, T1, Virtual<T2> > :
-      public T1, public virtual T2 {
+    class Type2 : public T1, public T2 {
     public:
-      template<typename A1>
-      Type(A1 a1) : T1(a1) {}
+      Type2(void) {} 
+
+     template<typename A1>
+      Type2(A1 a1) : T1(a1) {}
 
       template<typename A1, typename A2>
-      Type(A1 a1, A2 a2) : T1(a1, a2) {}
+      Type2(A1 a1, A2 a2) : T1(a1, a2) {}
+
+      void accept(Visitor &V) {
+	error("Inherit::accept called");
+      }
+    };
+
+    template<typename Visitor, typename T1, typename T2>
+    class Type2<Visitor, T1, Virtual<T2> > :
+      public T1, public virtual T2 {
+    public:
+      Type2(void) {}
+
+      template<typename A1>
+      Type2(A1 a1) : T1(a1) {}
+
+      template<typename A1, typename A2>
+      Type2(A1 a1, A2 a2) : T1(a1, a2) {}
 
       void accept(Visitor &V) {
 	error("Inherit::accept called");
@@ -47,11 +83,20 @@ namespace mirv {
   }
 
   template<typename Visitor>
-  class Inherit {
+  class Inherit2 {
   public:
     template<typename T1, typename T2>
     struct apply {
-      typedef detail::Type<Visitor, T1, T2> type;
+      typedef detail::Type2<Visitor, T1, T2> type;
+    };
+  };
+
+  template<typename Visitor>
+  class Inherit1 {
+  public:
+    template<typename T1>
+    struct apply {
+      typedef detail::Type1<Visitor, T1> type;
     };
   };
  }
--- src/core/ir/node.hh
+++ src/core/ir/node.hh
@@ -7,6 +7,7 @@
 #include <mirv/core/mem/heap.hh>
 #include <mirv/core/util/debug.hh>
 
+#include <boost/mpl/vector.hpp>
 #include <list>
 
 namespace mirv {
@@ -19,9 +20,52 @@ namespace mirv {
      virtual ~Node(void);
    };
 
+  template<typename Traits>
+  class Inner {
+  public:
+    class interface : public Traits::base_type {
+    public:
+      typedef typename ptr<typename Traits::Child>::type child_ptr;
+      typedef typename ptr<typename Traits::Child>::const_type const_child_ptr;
+
+     typedef typename Traits::iterator iterator;
+     typedef typename Traits::reverse_iterator reverse_iterator;
+     typedef typename Traits::const_iterator const_iterator;
+     typedef typename Traits::const_reverse_iterator const_reverse_iterator;
+
+      typedef typename Traits::size_type size_type;
+
+      virtual iterator begin(void) = 0;
+      virtual const_iterator begin(void) const = 0;
+      virtual reverse_iterator rbegin(void) = 0;
+      virtual const_reverse_iterator rbegin(void) const = 0;
+
+      virtual iterator end(void) = 0;
+      virtual const_iterator end(void) const = 0;
+      virtual reverse_iterator rend(void) = 0;
+      virtual const_reverse_iterator rend(void) const = 0;
+ 
+      virtual void push_back(child_ptr c) = 0;
+
+      virtual child_ptr front(void) = 0;
+      virtual const_child_ptr front(void) const = 0;
+
+      virtual child_ptr back(void) = 0;
+      virtual const_child_ptr back(void) const = 0;
+
+      virtual size_type size(void) const = 0;
+
+      virtual bool empty(void) const = 0;
+    };
+ 
+    typedef interface base_type;
+    typedef typename Traits::base_type visitor_base_type;
+    typedef boost::mpl::vector<> properties;
+ };
+
    // A node with children
-   template<typename Child, typename BaseType>
-   class InnerImpl : public BaseType {
+    template<typename Child, typename BaseType>
+    class InnerImpl : public BaseType {
    protected:
       typedef typename ptr<Child>::type child_ptr;
       typedef typename ptr<Child>::const_type const_child_ptr;
