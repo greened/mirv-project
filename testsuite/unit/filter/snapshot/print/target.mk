df8bf70fdfccc1b895d60bc38135d1892e00c497
David A. Greene <greened@waller.obbligato.org>
2009-12-14 19:25:39 -0600

    Fix exe link flags.
    Generate linker flags correctly.

--- testsuite/unit/filter/snapshot/print/target.mk
+++ testsuite/unit/filter/snapshot/print/target.mk
@@ -2,12 +2,13 @@ ifndef TESTSUITE_UNIT_FILTER_SNAPSHOT_PRINT_TARGET_MK
 TESTSUITE_UNIT_FILTER_SNAPSHOT_PRINT_TARGET_MK := 1
 
 libmirvfilter_print_test_SRCDIR := $(dir $(lastword $(MAKEFILE_LIST)))
-libmirvfilter_print_test_LIBS = $(libmirvfilter_print_LIBRARIES)
-libmirvfilter_print_test_INCLUDES = $(FINAL_BUILDDIR)/include
 
 include $(SRCDIR)/src/filter/snapshot/print/target.mk
 
 include $(BUILDTOOLS)/autounit.mk
+
+libmirvfilter_print_test_LIBS = $(filter %.a,$(libmirvfilter_print_LIBRARIES))
+libmirvfilter_print_test_INCLUDES = $(FINAL_BUILDDIR)/include
 
 $(call make_unittest,libmirvfilter_print_test,$(libmirvfilter_print_test_SRCDIR),$(libmirvfilter_print_test_LIBS),$(libmirvfilter_print_test_INCLUDES),,,,,$(libmirvfilter_print_test_LIBS) $(libmirvfilter_print_test_INCLUDES))
 
