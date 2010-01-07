8ad49365b07959decb778e512d0a90b7f44018a2
David A. Greene <greened@waller.obbligato.org>
2010-01-06 19:28:01 -0600

    Force block-derived statements to have block children
    If a block-derived statement is supplied a child that is not
    a block statement, create a block statement to hold it.

--- src/core/ir/statement.hh
+++ src/core/ir/statement.hh
@@ -1,6 +1,7 @@
 #ifndef mirv_core_ir_statement_hh
 #define mirv_core_ir_statement_hh
 
+#include <mirv/core/builder/make.hh>
 #include <mirv/core/ir/node.hh>
 #include <mirv/core/ir/expression.hh>
 #include <mirv/core/ir/inherit.hh>
