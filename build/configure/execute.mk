3b35c95683e7bb41466fa86acd15b217b80ab701
David A. Greene <greened@obbligato.org>
2010-01-07 23:05:23 -0600

    Simplify tests
    Remove the need to pass result file names to tests.  Generate
    the names using the unique tag instead.
    
    Add the ability to specify extra flags for compile and link tests.

--- build/configure/execute.mk
+++ build/configure/execute.mk
@@ -6,32 +6,32 @@ include $(BUILDTOOLS)/configure/define.mk
 
 define mc_execute_impl
 
-$$(call debug,call mc_execute $(1):$(2):$(3):$(4))
+$$(call debug,call mc_execute $(1):$(2):$(3))
 
-$$(call debug,[mc_execute] $(3): $(4))
+$(1)_execute_target = $(FINAL_BUILDDIR)/configure/$(1)_execute_result.mk
 
-include $(4)
+$$(call debug,[mc_execute] $$($(1)_execute_target): $(3))
 
-$(3): $(4)
-	$$(if $(4),$$(eval include $(4)))
+include $(3)
+
+$$($(1)_execute_target): $(3)
+	$$(if $(3),$$(eval include $(3)))
 	$$(call debug,CXX = $$(CXX))
-	$(QUIET)$$(call mc_define_create,$(1),$$(shell $$($(2))),$$@,$(4))
+	$(QUIET)$$(call mc_define_create,$(1),$$(shell $$($(2))),$$@,$(3))
 
-CONFIGURE_INCLUDES += $(3)
+CONFIGURE_INCLUDES += $$($(1)_execute_target)
 
 endef
 
 # $1: Variable to set with command output
 # $2: Command to execute
-# $3: File to create
-# $4: Dependency
-mc_execute = $(eval $(call mc_execute_impl,$(1),$(2),$(3),$(4)))
+# $3: Dependency
+mc_execute = $(eval $(call mc_execute_impl,$(1),$(2),$(3)))
 
 # $1: Variable to set with execute result
 # $2: Command to execute
 # $3: True action
 # $4: False action
-# $5: Include file to create (actions must create it)
-# $6: Dependency
-mc_try_execute = $(call mc_try,$(1),$(call bind4,$(1)_try_execute_bind,mc_execute,$(1),$(2),$(patsubst %.mk,%_execute.mk,$(5)),$(6)),$(3),$(4),$(5),$(patsubst %.mk,%_execute.mk,$(5)))
+# $5: Dependency
+mc_try_execute = $(call mc_try,$(1),$(call bind4,$(1)_try_execute_bind,mc_execute,$(1),$(2),$(5)),$(3),$(4),$(FINAL_BUILDDIR)/configure/$(1)_execute_result.mk)
 endif
