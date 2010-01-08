4ba372fb43d7c751dea66ab7a746f91194ea68a7
David A. Greene <greened@waller.obbligato.org>
2010-01-07 23:05:23 -0600

    Simplify tests
    Remove the need to pass result file names to tests.  Generate
    the names using the unique tag instead.
    
    Add the ability to specify extra flags for compile and link tests.

--- configure/compile.mk
+++ configure/compile.mk
@@ -8,12 +8,12 @@ include $(BUILDTOOLS)/configure/ls.mk
 
 define mc_compile_impl
 
-$$(call debug,[mc_compile_impl] mc_compile_impl("$(1)" "$(2)" "$(3)" "$(4)" "$(5)"))
+$$(call debug,[mc_compile_impl] mc_compile_impl("$(1)" "$(2)" "$(3)" "$(4)" "$(5)" "$(6)"))
 
-$(1)_OSRCFILE := $(FINAL_BUILDDIR)/configure/$(1)_compile_test.$(3)
-$(1)_OFILE := $$(patsubst %.$(3),%.o,$$($(1)_OSRCFILE))
+$(1)_OSRCFILE := $(FINAL_BUILDDIR)/configure/$(1)_compile_test.$(4)
+$(1)_OFILE := $$(patsubst %.$(4),%.o,$$($(1)_OSRCFILE))
 
-$(1)_compile_source := $$($(2))
+$(1)_compile_source := $$($(3))
 
 $$(call debug,[mc_compile_impl] $(1)_OSRCFILE = $$($(1)_OSRCFILE))
 $$(call debug,[mc_compile_impl] $(1)_OFILE    = $$($(1)_OFILE))
@@ -22,72 +22,78 @@ $$(call debug,[mc_compile_impl] $(1)_OFILE    = $$($(1)_OFILE))
 $$($(1)_OSRCFILE):
 	$(QUIET)echo "$$($(1)_compile_source)" > $$@
 
-$$(call debug,[mc_compile_impl] $$($(1)_OFILE): $$($(1)_OSRCFILE) $(5))
+$$(call debug,[mc_compile_impl] $$($(1)_OFILE): $$($(1)_OSRCFILE) $(6))
 
-include $(5)
+include $(6)
 
 .PRECIOUS: $$($(1)_OFILE)
 .SECONDEXPANSION:
-$$($(1)_OFILE): $$($(1)_OSRCFILE) $(5)
-	$$(if $(5),$$(eval include $(5)))
-	-$(QUIET)$$($(1)) -c $$< -o $$@
+$$($(1)_OFILE): $$($(1)_OSRCFILE) $(6)
+	$$(if $(6),$$(eval include $(6)))
+	-$(QUIET)$$($(2)) $(5) -c $$< -o $$@
 
-$$(call debug,[mc_compile_impl] $(4): $$($(1)_OFILE))
+$(1)_compile_target = $(FINAL_BUILDDIR)/configure/$(1)_compile_result.mk
 
-#	$(QUIET)$$(if $$(wildcard $$(<)),$$(call mc_define,$(1)_compile,yes,$$@),$$(call mc_define,$(1)_compile,,$$@))
+$$(call debug,[mc_compile_impl] $$($(1)_compile_target): $$($(1)_OFILE))
 
-$(4): $$($(1)_OFILE)
+$$($(1)_compile_target): $$($(1)_OFILE)
 	$(QUIET)if [ -e $$(<) ]; then \
 	          $$(call mc_define_append,$(1)_compile,yes,$$@); \
 	        else \
 	          $$(call mc_define_append,$(1)_compile,,$$@); \
 	        fi
 
-CONFIGURE_INCLUDES += $(4)
+CONFIGURE_INCLUDES += $$($(1)_compile_target)
 
 endef
 
-# $1: The name of the variable holding the compiler location
-# $2: The source code to compile
-# $3: The source code file extension
-# $4: Output file to create
-# $5: Dependency
+# $1: A unique name
+# $2: The name of the variable holding the compiler location
+# $3: The source code to compile
+# $4: The source code file extension
+# $5: Extra compiler flags
+# $6: Dependency
 # Set $(1)_compile to yes or empty
-mc_compile = $(eval $(call mc_compile_impl,$(1),$(2),$(3),$(4),$(5)))
+mc_compile = $(eval $(call mc_compile_impl,$(1),$(2),$(3),$(4),$(5),$(6)))
 
-# $1: The source code to compile
-# $2: Output file to create
-# $3: Dependence
-# Set CC_compile to yes or empty
-mc_compile_c = $(call mc_compile,CC,$(1),c,$(2),$(3))
-
-# $1: The source code to compile
-# $2: Output file to create
-# $3: Dependence
-# Set CXX_compile to yes or empty
-mc_compile_cxx = $(call mc_compile,CXX,$(1),cc,$(2),$(3))
-
-# $1: The name of the variable holding the compiler location
+# $1: A unique name
 # $2: The source code to compile
-# $3: True action
-# $4: False action
-# $5: The source code file extension
-# $6: Include file to create (actions must create it)
-# $7: Dependence
-mc_try_compile = $(call mc_try,$(1)_compile,$(call bind5,$(1)_compile_bind,mc_compile,$(1),$(2),$(5),$(patsubst %.mk,%_compile.mk,$(6)),$(7)),$(3),$(4),$(6),$(patsubst %.mk,%_compile.mk,$(6)))
+# $3: Extra compiler flags
+# $4: Dependence
+# Set CC_compile to yes or empty
+mc_compile_c = $(call mc_compile,$(1),CC,$(2),c,$(3),$(4))
 
-# $1: The source code to compile
-# $2: True action
-# $3: False action
-# $4: Include file to create (actions must create it)
-# $5: Dependence
-mc_try_compile_c = $(call mc_try,CC_compile,$(call bind3,CC_compile_bind,mc_compile_c,$(1),$(patsubst %.mk,%_compile.mk,$(4)),$(5)),$(2),$(3),$(4),$(patsubst %.mk,%_compile.mk,$(4)))
+# $1: A unique name
+# $2: The source code to compile
+# $3: Extra compiler flags
+# $4: Dependence
+# Set CXX_compile to yes or empty
+mc_compile_cxx = $(call mc_compile,$(1),CXX,$(2),cc,$(3),$(4))
 
-# $1: The source code to compile
-# $2: True action
-# $3: False action
-# $4: Include file to create (actions must create it)
-# $5: Dependence
-mc_try_compile_cxx = $(call mc_try,CXX_compile,$(call bind3,CXX_compile_bind,mc_compile_cxx,$(1),$(patsubst %.mk,%_compile.mk,$(4)),$(5)),$(2),$(3),$(4),$(patsubst %.mk,%_compile.mk,$(4)))
+# $1: A unique name
+# $2: The name of the variable holding the compiler location
+# $3: The source code to compile
+# $4: The source code file extension
+# $5: Extra compiler flags
+# $6: True action
+# $7: False action
+# $8: Dependence
+mc_try_compile = $(call debug,[mc_try_compile] mc_try_compile("$(1)" "$(2)" "$(3)" "$(4)" "$(5)" "$(6)" "$(7)" "$(8)"))$(call mc_try,$(1)_compile,$(call bind6,$(1)_compile_bind,mc_compile,$(1),$(2),$(3),$(4),$(5),$(8)),$(6),$(7),$(FINAL_BUILDDIR)/configure/$(1)_compile_result.mk)
+
+# $1: A unique tag
+# $2: The source code to compile
+# $3: Extra compiler flags
+# $4: True action
+# $5: False action
+# $6: Dependence
+mc_try_compile_c = $(call mc_try_compile,$(1),CC,$(2),c,$(3),$(4),$(5),$(6))
+
+# $1: A unique name
+# $2: The source code to compile
+# $3: Extra compiler flags
+# $4: True action
+# $5: False action
+# $6: Dependence
+mc_try_compile_cxx = $(call mc_try_compile,$(1),CXX,$(2),cc,$(3),$(4),$(5),$(6))
 
 endif
--- configure/executable.mk
+++ configure/executable.mk
@@ -4,36 +4,39 @@ EXECUTABLE_MK := 1
 include $(BUILDTOOLS)/configure/compile.mk
 include $(BUILDTOOLS)/configure/link.mk
 
-# $1: The name of the variable holding the compiler location
+# $1: A unique name
+# $2: The name of the variable holding the compiler location
+# $3: Source code to compile and link
+# $4: The source code file extension
+# $5: Extra compiler flags
+# $6: Extra linker flags
+# $7: Compile true action
+# $8: Compile false action
+# $9: Link true action
+# $10: Link false action
+# $11: Dependence
+mc_try_executable = $(call debug,[mc_try_executable] mc_try_executable("$(1)" "$(2)" "$(3)" "$(4)" "$(5)" "$(6)" "$(7)" "$(8)" "$(9)" "$(10)" "$(11)"))$(call mc_try_compile,$(1)_executable,$(2),$(3),$(4),$(5),$(7),$(8),$(11))$(call mc_try_link,$(1)_executable,$(2),$(FINAL_BUILDDIR)/configure/$(1)_executable_compile_test.o,$(6),$(9),$(10),$(FINAL_BUILDDIR)/configure/$(1)_executable_compile_result.mk)
+
+# $1: A unique name
 # $2: Source code to compile and link
-# $3: Compile true action
-# $4: Compile false action
-# $5: Link true action
-# $6: Link false action
-# $7: The source code file extension
-# $8: Compile include file to create (actions must create it) 
-# $9: Link include file to create (actions must create it)
-# $10: Dependence
-mc_try_executable = $(call mc_try_compile,$(1),$(2),$(3),$(4),$(7),$(8),$(10))$(call mc_try_link,$(1),$(1)_compile_test.o,$(5),$(6),$(9),$(10))
+# $3: Extra compiler flags
+# $4: Extra linker flags
+# $5: Compile true action
+# $6: Compile false action
+# $7: Link true action
+# $8: Link false action
+# $9: Dependence
+mc_try_executable_c = $(call mc_try_executable,$(1),CC,$(2),c,$(3),$(4),$(5),$(6),$(7),$(8),$(9))
 
-# $1: Source code to compile and link
-# $2: Compile true action
-# $3: Compile false action
-# $4: Link true action
-# $5: Link false action
-# $6: Compile include file to create (actions must create it) 
-# $7: Link include file to create (actions must create it)
-# $8: Dependence
-mc_try_executable_c = $(call mc_try_compile_c,$(1),$(2),$(3),$(6),$(8))$(call mc_try_link_c,$(FINAL_BUILDDIR)/configure/CC_compile_test.o,$(4),$(5),$(7),$(8))
-
-# $1: Source code to compile and link
-# $2: Compile true action
-# $3: Compile false action
-# $4: Link true action
-# $5: Link false action
-# $6: Compile include file to create (actions must create it)
-# $7: Link include file to create (actions must create it)
-# $8: Dependence
-mc_try_executable_cxx = $(call mc_try_compile_cxx,$(1),$(2),$(3),$(6),$(8))$(call mc_try_link_cxx,$(FINAL_BUILDDIR)/configure/CXX_compile_test.o,$(4),$(5),$(7),$(6))
+# $1: A unque name
+# $2: Source code to compile and link
+# $3: Extra compiler flags
+# $4: Extra linker flags
+# $5: Compile true action
+# $6: Compile false action
+# $7: Link true action
+# $8: Link false action
+# $9: Dependence
+mc_try_executable_cxx = $(call mc_try_executable,$(1),CXX,$(2),cc,$(3),$(4),$(5),$(6),$(7),$(8),$(9))
 
 endif
