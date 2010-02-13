08cb8cd9ef290ed9d9cd29945f99470ceca12fbe
David A. Greene <greened@waller.obbligato.org>
2010-02-12 22:04:37 -0600

    Add symbol tests.
    Add tests for function and variable construction.

---
+++ testsuite/unit/Core/Builder/function.cc
@@ -0,0 +1,71 @@
+// Test building of functions.
+//
+// STDOUT: fdef testfunc {
+// STDOUT:    {
+// STDOUT:       assign
+// STDOUT:          vref a
+// STDOUT:          +
+// STDOUT:             vref a
+// STDOUT:             vref b
+// STDOUT:    }
+// STDOUT: }
+
+#include <mirv/Core/IR/IR.hpp>
+#include <mirv/Core/Builder/Builder.hpp>
+#include <mirv/Filter/Snapshot/Print/Print.hpp>
+
+using mirv::Symbol;
+using mirv::Module;
+using mirv::Function;
+using mirv::Variable;
+using mirv::Type;
+using mirv::TypeBase;
+using mirv::Integral;
+using mirv::FunctionType;
+using mirv::Node;
+using mirv::Base;
+using mirv::ptr;
+using mirv::PrintFilter;
+using mirv::make;
+
+namespace Builder = mirv::Builder;
+
+using Builder::if_;
+using Builder::do_;
+using Builder::function;
+using Builder::var;
+
+int main(void)
+{
+  ptr<Symbol<Module> >::type module = make<Symbol<Module> >("testmodule");
+
+  ptr<Symbol<Type<TypeBase> > >::type functype =
+    make<Symbol<Type<FunctionType> > >("functype");
+  module->typePushBack(functype);
+
+  ptr<Symbol<Type<TypeBase> > >::type inttype =
+    make<Symbol<Type<Integral> > >("int32", 32);
+  module->typePushBack(inttype);
+
+  ptr<Symbol<Variable> >::type asym = make<Symbol<Variable> >("a", inttype);
+  module->variablePushBack(asym);
+
+  ptr<Symbol<Variable> >::type bsym = make<Symbol<Variable> >("b", inttype);
+  module->variablePushBack(bsym);
+
+  Builder::VariableTerminal a = {{"a"}};
+  Builder::VariableTerminal b = {{"b"}};
+
+  ptr<Node<Base> >::type code =
+    Builder::translate(module, ptr<Symbol<Function> >::type(),
+      function["testfunc"].type["functype"] [
+       	a = a + b
+      ]
+    );
+
+  PrintFilter print(std::cout);
+  
+  print(code);
+
+  return(0);
+}
---
+++ testsuite/unit/Core/Builder/var.cc
@@ -0,0 +1,57 @@
+// Test building of variables.
+//
+// STDOUT: fdef testfunc {
+// STDOUT:    vdecl a int32
+// STDOUT:    {
+// STDOUT:
+// STDOUT:    }
+// STDOUT: }
+
+#include <mirv/Core/IR/IR.hpp>
+#include <mirv/Core/Builder/Builder.hpp>
+#include <mirv/Filter/Snapshot/Print/Print.hpp>
+
+using mirv::Symbol;
+using mirv::Module;
+using mirv::Function;
+using mirv::Variable;
+using mirv::Type;
+using mirv::TypeBase;
+using mirv::Integral;
+using mirv::FunctionType;
+using mirv::Node;
+using mirv::Base;
+using mirv::ptr;
+using mirv::PrintFilter;
+using mirv::make;
+
+namespace Builder = mirv::Builder;
+
+using Builder::var;
+using Builder::function;
+
+int main(void)
+{
+  ptr<Symbol<Module> >::type module = make<Symbol<Module> >("testmodule");
+
+  ptr<Symbol<Type<TypeBase> > >::type functype =
+    make<Symbol<Type<FunctionType> > >("functype");
+  module->typePushBack(functype);
+
+  ptr<Symbol<Type<TypeBase> > >::type inttype =
+    make<Symbol<Type<Integral> > >("int32", 32);
+  module->typePushBack(inttype);
+
+  ptr<Node<Base> >::type code =
+    Builder::translate(module, ptr<Symbol<Function> >::type(),
+      function["testfunc"].type["functype"] [
+	var["a"].type["int32"]
+      ]
+    );
+
+  PrintFilter print(std::cout);
+  
+  print(code);
+
+  return(0);
+}
