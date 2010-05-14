e7e11e539ff7f929c1596cd2b9656c9e3c9f9fca
David A. Greene <greened@obbligato.org>
2010-05-14 17:23:10 -0500

    Update doxygen rules
    Make doxygen rules dependent on source files.  Pass user-added dependencies
    to generated doxygen rules.  Add more debug output.

--- doc/target.mk
+++ doc/target.mk
@@ -5,7 +5,13 @@ doc_SRCDIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 include $(BUILDTOOLS)/autodox.mk
 
-$(call make_doxyconfig,libmirv_core,$(doc_SRCDIR))
+$(call debug,[doc] SRCDIR = $(SRCDIR))
+
+doc_SRCS := $(shell find $(SRCDIR)/src -regextype posix-extended -regex ".*\.cpp|.*\.c" -print)
+
+$(call debug,[doc] SRCS = $(doc_SRCS))
+
+$(call make_doxyconfig,mirv,$(doc_SRCDIR),$(doc_SRCS))
 
 TARGETS += $(libmirv_core_DOXY_STAMPS)
 
