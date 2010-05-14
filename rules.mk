c26833b68baf045076cb1215aee26e0e46d07c90
David A. Greene <greened@waller.obbligato.org>
2010-05-14 17:25:22 -0500

    Add C++0x Flags
    Enable C++0x support when compiling.

--- rules.mk
+++ rules.mk
@@ -4,12 +4,16 @@ include $(BUILDTOOLS)/configure/touch.mk
 IFLAGS = -I$(SRCDIR)/src/Library -I$(FINAL_BUILDDIR)/include
 DFLAGS = -DBOOST_MPL_LIMIT_VECTOR_SIZE=40 -DBOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
 
-debug_CFLAGS := -g -Wall -Werror
-debug_CXXFLAGS := -g -Wall -Werror
+#debug_CFLAGS := -g -Wall -Werror
+#debug_CXXFLAGS := -g -Wall -Werror
+debug_CFLAGS := -g -Wall
+debug_CXXFLAGS := -g -Wall -std=c++0x
 debug_CPPFLAGS = -DDEBUG $(IFLAGS) $(DFLAGS)
 
-opt_CFLAGS := -O2 -Wall -Werror
-opt_CXXFLAGS := -O2 -Wall -Werror
+#opt_CFLAGS := -O2 -Wall -Werror
+#opt_CXXFLAGS := -O2 -Wall -Werror
+opt_CFLAGS := -O2 -Wall
+opt_CXXFLAGS := -O2 -Wall -std=c++0x
 opt_CPPFLAGS = -O2 $(IFLAGS) $(DFLAGS)
 
 INCLUDE_LINK = $(FINAL_BUILDDIR)/include/mirv
