bd7b1fc3b98d25278e5e09a1f3178baf3d6ab3d8
David A. Greene <greened@waller.obbligato.org>
2010-07-24 04:06:57 -0500

    Add module codegen test
    This tests the codegen filter on a simple module.

---
+++ testsuite/unit/Filter/Snapshot/Codegen/LLVM/Module.cpp
@@ -0,0 +1,88 @@
+// Test building of variables.
+//
+
+#include <mirv/Core/IR/Module.hpp>
+#include <mirv/Core/IR/Function.hpp>
+#include <mirv/Core/IR/Variable.hpp>
+#include <mirv/Core/IR/ArrayType.hpp>
+#include <mirv/Core/IR/FloatingType.hpp>
+#include <mirv/Core/IR/FunctionType.hpp>
+#include <mirv/Core/IR/IntegralType.hpp>
+#include <mirv/Core/IR/PointerType.hpp>
+#include <mirv/Core/IR/StructType.hpp>
+#include <mirv/Core/IR/Relational.hpp>
+#include <mirv/Core/IR/Arithmetic.hpp>
+#include <mirv/Core/IR/Control.hpp>
+#include <mirv/Core/IR/Mutating.hpp>
+#include <mirv/Core/Builder/Builder.hpp>
+#include <mirv/Core/Builder/ModuleGrammar.hpp>
+#include <mirv/Core/Builder/Translate.hpp>
+#include <mirv/Core/Builder/Domain.hpp>
+#include <mirv/Filter/Snapshot/Codegen/LLVM/LLVM.hpp>
+
+#include <llvm/Module.h>
+
+#include <iostream>
+
+using mirv::Symbol;
+using mirv::Module;
+using mirv::Function;
+using mirv::Variable;
+using mirv::Type;
+using mirv::TypeBase;
+using mirv::Integral;
+using mirv::FunctionType;
+using mirv::Node;
+using mirv::Base;
+using mirv::ptr;
+using mirv::LLVMCodegenFilter;
+using mirv::make;
+
+namespace Builder = mirv::Builder;
+
+using Builder::var;
+using Builder::function;
+using Builder::module;
+using Builder::void_;
+using Builder::int_;
+using Builder::do_;
+using Builder::if_;
+
+int main(void)
+{
+  Builder::VariableTerminal a = {{"a"}};
+  Builder::VariableTerminal b = {{"b"}};
+  Builder::VariableTerminal c = {{"c"}};
+  Builder::VariableTerminal d = {{"d"}};
+
+  ptr<Node<Base> >::type code =
+    Builder::translateWithGrammar<Builder::ModuleBuilder>(
+      module["testmodule"] [
+	var[a].type[int_(32)],
+	var[b].type[int_(32)],
+
+        function["testfunc"].type[void_()] [
+          var[c].type[int_(32)],
+          var[d].type[int_(32)],
+
+          do_[
+            a = a + b,
+            if_(b > c) [
+              a = a + d
+            ].else_[
+              a = a + c
+            ]
+          ].while_(a < c),
+          c = a
+        ]
+      ]
+    );
+
+  LLVMCodegenFilter codegen;
+  codegen(code);
+  llvm::Module *TheModule = codegen.getModule();
+
+  std::cout << *TheModule;
+
+  return(0);
+}
