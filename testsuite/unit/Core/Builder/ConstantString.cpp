200aa510abd87fa6dfc5538102b3dafcc914521c
David A. Greene <greened@obbligato.org>
2010-12-10 09:16:52 -0600

    String Constant Test
    Add a test for printing string constants.

---
+++ testsuite/unit/Core/Builder/ConstantString.cpp
@@ -0,0 +1,82 @@
+// Test building of string constants.
+//
+// STDOUT: mdef testmodule {
+// STDOUT:    fdecl printf int32 (int8 *, ...)
+// STDOUT:    vdecl __str0__ int8[15] "Hello, World!\n\0"
+// STDOUT:    fdef testfunc {
+// STDOUT:       vdecl num int32
+// STDOUT:       {
+// STDOUT:          call
+// STDOUT:             fref printf
+// STDOUT:             vref num
+// STDOUT:             addrOf
+// STDOUT:                aref
+// STDOUT:                   vref __str0__
+// STDOUT:                   cref 0
+// STDOUT:       }
+// STDOUT:    }
+// STDOUT: }
+
+#include <mirv/Core/IR/Module.hpp>
+#include <mirv/Core/IR/Function.hpp>
+#include <mirv/Core/IR/Variable.hpp>
+#include <mirv/Core/IR/Constant.hpp>
+#include <mirv/Core/IR/ArrayType.hpp>
+#include <mirv/Core/IR/FloatingType.hpp>
+#include <mirv/Core/IR/FunctionType.hpp>
+#include <mirv/Core/IR/IntegralType.hpp>
+#include <mirv/Core/IR/PointerType.hpp>
+#include <mirv/Core/IR/StructType.hpp>
+#include <mirv/Core/IR/Relational.hpp>
+#include <mirv/Core/IR/Arithmetic.hpp>
+#include <mirv/Core/IR/Control.hpp>
+#include <mirv/Core/IR/Mutating.hpp>
+#include <mirv/Core/Builder/Builder.hpp>
+#include <mirv/Core/Builder/ModuleGrammar.hpp>
+#include <mirv/Core/Builder/Translate.hpp>
+#include <mirv/Core/Builder/Domain.hpp>
+#include <mirv/Filter/Snapshot/Print/Print.hpp>
+
+using mirv::Symbol;
+using mirv::Module;
+using mirv::Function;
+using mirv::Variable;
+using mirv::Type;
+using mirv::TypeBase;
+using mirv::Integral;
+using mirv::FunctionType;
+using mirv::Node;
+using mirv::Base;
+using mirv::ptr;
+using mirv::PrintFilter;
+using mirv::make;
+
+namespace Builder = mirv::Builder;
+
+using Builder::var;
+using Builder::func;
+using Builder::module;
+using Builder::void_;
+using Builder::int_;
+using Builder::do_;
+
+int main(void)
+{
+  Builder::VariableTerminal i = {{"i"}};
+
+  ptr<Node<Base> >::type code =
+    Builder::translateWithGrammar<Builder::ModuleBuilder>(
+      module["testmodule"] [
+        func["testfunc"].type[void_()] [
+          var[i].type[int_(32)],
+          i = 0
+        ]
+      ]
+    );
+
+  PrintFilter print(std::cout);
+  
+  print(code);
+
+  return(0);
+}
