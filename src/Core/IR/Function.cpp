3600934cf2ef0c5b3ba9d41e3fb5035edd397af6
David A. Greene <greened@obbligato.org>
2011-01-06 08:41:04 -0600

    Set symbol parents
    Set parent pointers on symbols when adding them to parents.

--- src/Core/IR/Function.cpp
+++ src/Core/IR/Function.cpp
@@ -8,11 +8,13 @@ namespace mirv {
       StatementPtr newStmt = (dyn_cast<Statement<Block> >(stmt) ?
                               stmt : boost::static_pointer_cast<Statement<Base> >(mirv::make<Statement<Block> >(stmt)));
       StatementBaseType::push_back(newStmt);
+      newStmt->setParent(this->getSharedHandle());
     }
     else {
       ptr<Statement<Block> >::type block =
         safe_cast<Statement<Block> >(StatementBaseType::front());
       block->push_back(stmt);
+      block->setParent(this->getSharedHandle());
     }
   }
 }
