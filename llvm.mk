066a370408ed056bfdb463c7d81df357635120b0
David Greene <greened@obbligato.org>
2011-05-31 20:28:50 -0500

    Clean Up Library Messages
    Do not output messages for defining variables related to
    Boost and LLVM library discovery.

--- llvm.mk
+++ llvm.mk
@@ -25,8 +25,8 @@ override LLVM_LINK_FLAGS = -L/usr/lib/llvm-2.8/lib -rdynamic -Xlinker -rpath -Xl
 override LLVM_COMPILE_FLAGS = -I/usr/lib/llvm-2.8/include -fpermissive -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS
 override LLVM_COMPILE_ERROR = $(call mc_error,Cannot find LLVM libraries)
 override LLVM_LINK_ERROR = $(call mc_error,Cannot link to LLVM libraries)
-override LLVM_COMPILE_TRUE = $(call mc_define_append,CONFIG_LLVM_COMPILE,yes,$@); $(call mc_info_append,C++ compiler can find LLVM headers,$@)
-override LLVM_LINK_TRUE = $(call mc_define_append,CONFIG_LLVM_LINK,yes,$@); $(call mc_info_append,C++ linker can link to LLVM libraries,$@)
+override LLVM_COMPILE_TRUE = $(call mc_define_append_nomsg,CONFIG_LLVM_COMPILE,yes,$@); $(call mc_info_append,C++ compiler can find LLVM headers,$@)
+override LLVM_LINK_TRUE = $(call mc_define_append_nomsg,CONFIG_LLVM_LINK,yes,$@); $(call mc_info_append,C++ linker can link to LLVM libraries,$@)
 override LLVM_DEP_MK = $(FINAL_BUILDDIR)/configure/CXX.mk
 
 $(call mc_try_library_cxx,LLVM,LLVM_SOURCE,LLVM_LIBRARY,$(LLVM_COMPILE_FLAGS),$(LLVM_LINK_FLAGS),LLVM_COMPILE_TRUE,LLVM_COMPILE_ERROR,LLVM_LINK_TRUE,LLVM_LINK_ERROR,$(LLVM_DEP_MK))
