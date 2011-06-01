612fc4e8f4d691a34ad60796700d3eb136cc59ec
David Greene <greened@obbligato.org>
2011-05-31 20:24:11 -0500

    Clean Up Compiler Messages
    Get rid of some messages for intermediate compiler control
    variable definitions.

--- configure/cc.mk
+++ configure/cc.mk
@@ -24,16 +24,16 @@ $(call mc_target_tool,CC,gcc cc)
 override C_COMPILE_SOURCE := int main(void) { return(0); }
 override C_COMPILE_ERROR = $(call mc_error,C compiler cannot create objects)
 override C_LINK_ERROR = $(call mc_error,C compiler cannot create executables)
-override C_COMPILE_COMPILE_TRUE = $(call mc_define,CONFIG_CC_COMPILE,yes,$@)
-override C_COMPILE_LINK_TRUE = $(call mc_define,CONFIG_CC_LINK,yes,$@)
+override C_COMPILE_COMPILE_TRUE = $(call mc_define_nomsg,CONFIG_CC_COMPILE,yes,$@)
+override C_COMPILE_LINK_TRUE = $(call mc_define_nomsg,CONFIG_CC_LINK,yes,$@)
 override C_COMPILE_DEP_MK = $(FINAL_BUILDDIR)/configure/CC.mk
 
 $(call mc_try_executable_c,CC_executable,C_COMPILE_SOURCE,,,C_COMPILE_COMPILE_TRUE,C_COMPILE_ERROR,C_COMPILE_LINK_TRUE,C_LINK_ERROR,$(C_COMPILE_DEP_MK))
 
 # See if this is gcc
 C_EXECUTE_RUN = $(CC) --version | grep "gcc (GCC)"
-C_EXECUTE_TRUE = $(call mc_info,Using gcc)$(call mc_define,CONFIG_HAVE_GCC,yes,$@)
-C_EXECUTE_FALSE = $(call mc_define,CONFIG_HAVE_GCC,,$@)
+C_EXECUTE_TRUE = $(call mc_info,Using gcc)$(call mc_define_nomsg,CONFIG_HAVE_GCC,yes,$@)
+C_EXECUTE_FALSE = $(call mc_define_nomsg,CONFIG_HAVE_GCC,,$@)
 C_EXECUTE_DEP_MK = $(FINAL_BUILDDIR)/configure/CC.mk
 
 $(call mc_try_execute,CC_EXECUTE,C_EXECUTE_RUN,C_EXECUTE_TRUE,C_EXECUTE_FALSE,$(C_EXECUTE_DEP_MK))
