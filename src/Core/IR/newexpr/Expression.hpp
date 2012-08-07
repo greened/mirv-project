78617a88e6b45675a3f679e707f121f812891c11
David A. Greene <greened@obbligato.org>
2012-08-06 19:45:42 -0500

    Add Generic IR Node
    Add a generic IR node type to provide common interfaces.

--- src/Core/IR/newexpr/Expression.hpp
+++ src/Core/IR/newexpr/Expression.hpp
@@ -3,59 +3,51 @@
 
 #include <Core/Containers/Vector.hpp>
 #include <Core/IR/Index.hpp>
+#include <Core/IR/Node.hpp>
 
 #include <boost/range/iterator_range.hpp>
 
 namespace mirv {
-  enum ExpressionKind {
-    Null,
-
-    // Arithmetic
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    Modulus,
-    Negate,
-
-    // Logical
-    LogicalAnd,
-    LogicalOr,
-    LogicalNot,
-
-    // Bitwise
-    BitwiseAnd,
-    BitwiseOr,
-    BitwiseComplement,
-    BitwiseXor,
-    ShiftLeft,
-    ArithmeticShiftRight,
-    LogicalShiftRight,
-
-    // Relational
-    LessThan,
-    LessThanOrEqual,
-    Equal,
-    NotEqual,
-    GreaterThanOrEqual,
-    GreaterThan,
-
-    // Reference
-    Reference,
-    Load,
-    TuplePointer
-  };
-
-  class Expression {
-  private:
-    ExpressionKind kind;
-
-    typedef Vector<Index<Expression>> IndexVector;
-    IndexVector children;
-
+  class Expression : public Node<Expression> {
   public:
-    typedef boost::iterator_range<IndexVector::iterator> range;
-    typedef boost::iterator_range<VectorType::const_iterator> const_range;
+    enum Kind {
+      Null,
+
+      // Arithmetic
+      Add,
+      Subtract,
+      Multiply,
+      Divide,
+      Modulus,
+      Negate,
+
+      // Logical
+      LogicalAnd,
+      LogicalOr,
+      LogicalNot,
+
+      // Bitwise
+      BitwiseAnd,
+      BitwiseOr,
+      BitwiseComplement,
+      BitwiseXor,
+      ShiftLeft,
+      ArithmeticShiftRight,
+      LogicalShiftRight,
+
+      // Relational
+      LessThan,
+      LessThanOrEqual,
+      Equal,
+      NotEqual,
+      GreaterThanOrEqual,
+      GreaterThan,
+
+      // Reference
+      Reference,
+      Load,
+      TuplePointer
+    };
   };
 }
 
