0d92e719db64a5f047093abdc464b39c862ed9a9
David A. Greene <greened@obbligato.org>
2012-09-06 19:58:29 -0500

    Derive Variable from Symbol
    Have Variables take a name and pass them to Symbol.

--- src/newir/Core/IR/Variable.hpp
+++ src/newir/Core/IR/Variable.hpp
@@ -1,15 +1,17 @@
-#ifndef mirv_Core_IR_Variable_hp
+#ifndef mirv_Core_IR_Variable_hpp
 #define mirv_Core_IR_Variable_hpp
 
 #include <Core/IR/detail/IRNode.hpp>
 
 namespace mirv {
   // Represent a function or routine.
-  class Variable {
+  class Variable : public Symbol<Variable> {
   public:
     enum {
       IRKind = detail::IRNode::Variable;
     };
+
+    Variable(const std::string &name) : Symbol(name) {}
   };
 }
 
