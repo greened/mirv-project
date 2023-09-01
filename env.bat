51af277378bee691b00460528bcd5c685098c869
David Greene <dag@waller.obbligato.org>
2023-09-01 00:44:04 -0500

    [Licence] Copyright and license files
    Apply the GNU Affero General Public License version 3 to all relevant files.
    Include copyright and license notices in each and a COPYING file containing the
    entire text of the license.

--- env.bat
+++ env.bat
@@ -1,3 +1,20 @@
+REM Copyright 2023 David A. Greene
+
+REM This file is part of the MIRV project.
+
+REM MIRV is free software: you can redistribute it and/or modify it under the
+REM terms of the GNU Affero General Public License as published by the Free
+REM Software Foundation, either version 3 of the License, or (at your option)
+REM any later version.
+
+REM MIRV is distributed in the hope that it will be useful, but WITHOUT ANY
+REM WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+REM FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+REM details.
+
+REM You should have received a copy of the GNU General Public License along with
+REM Foobar. If not, see <https://www.gnu.org/licenses/>.
+
 @echo off
 
 for /f "tokens=*" %%G in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -Property InstallationPath') do ^
--- mirv/testsuite/unit/Core/Builder/CallStatementVoid.cpp
+++ mirv/testsuite/unit/Core/Builder/CallStatementVoid.cpp
@@ -1,3 +1,20 @@
+// Copyright 2023 David A. Greene
+
+// This file is part of the MIRV project.
+
+// MIRV is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+
+// MIRV is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+
+// You should have received a copy of the GNU General Public License along with
+// Foobar. If not, see <https://www.gnu.org/licenses/>.
+
 // Test building of function calls combined with another statement.
 // STDOUT: fdef testfunc {
 // STDOUT:    vdecl __ct0__ int32
